39d5c39cf6134e117af565edd872998633e6017d
Hegazian <38352684+Hegazian@users.noreply.github.com>
2021-08-16 18:29:39 +0200

    Update for_sum.c

--- Loops in c/for_sum.c
+++ Loops in c/for_sum.c
@@ -1,3 +1,5 @@
+/*	This C file is Written by Abdelrahman Hegazy email:abdelrahmanhegazy70@gmail.com gitgub:https://github.com/Hegazian	*/
+
 /*
 	Write a program in C to read 10 numbers
 	from the user and find their summation and average

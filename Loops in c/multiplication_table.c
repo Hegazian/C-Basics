b93818b5fc0d4806f08c09227497ca338ef5247b
Hegazian <38352684+Hegazian@users.noreply.github.com>
2021-08-16 18:30:42 +0200

    Update multiplication_table.c

--- Loops in c/multiplication_table.c
+++ Loops in c/multiplication_table.c
@@ -1,3 +1,5 @@
+/*	This C file is Written by Abdelrahman Hegazy email:abdelrahmanhegazy70@gmail.com gitgub:https://github.com/Hegazian	*/
+
 /*	Write a program in C to display the
 	multiplication table of a given integer.
 */

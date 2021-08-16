5f170ab69a68a57b29a6d04c04e7e3a56f962c5b
Hegazian <38352684+Hegazian@users.noreply.github.com>
2021-08-16 18:27:15 +0200

    Update do_while.c

--- Loops in c/do_while.c
+++ Loops in c/do_while.c
@@ -1,3 +1,5 @@
+/*	This C file is Written by Abdelrahman Hegazy email:abdelrahmanhegazy70@gmail.com gitgub:https://github.com/Hegazian	*/
+
 /* 	do while loop is used to apply block at least once.
 	it Apply the loop once then check the condition
 	and repeat the loop as long as the condition is true.
